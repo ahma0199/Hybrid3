bf0f9dc9a480b5cd7e531827a9553a129f87e3e3
Hena Ahmadzai <ahma0199@algonquinlive.com>
2023-10-12 15:52:07 -0400

    new dart hybrid

---
+++ bin/main.dart
@@ -0,0 +1,30 @@
+import '../lib/students.dart';
+
+void main() {
+  List<Map<String, String>> jsonData = [
+    {
+      "first": "Steve",
+      "last": "Griffith",
+      "email": "griffis@algonquincollege.com"
+    },
+    {"first": "Adesh", "last": "Shah", "email": "shaha@algonquincollege.com"},
+    {
+      "first": "Tony",
+      "last": "Davidson",
+      "email": "davidst@algonquincollege.com"
+    },
+    {
+      "first": "Adam",
+      "last": "Robillard",
+      "email": "robilla@algonquincollege.com"
+    }
+  ];
+
+  Students students = Students(jsonData);
+
+  students.sort("first");
+  students.output();
+  students
+      .plus({"first": "New", "last": "Student", "email": "new@student.com"});
+  students.remove("last");
+}
